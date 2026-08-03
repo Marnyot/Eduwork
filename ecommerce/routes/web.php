c2d2c9899fffed150db5ba4dbfab201166a2229a
Marnyot <marioardhana37@gmail.com>
2026-08-03 19:19:32 +0700

    feat(ecommerce): add /products /cart /checkout routes (sesi 10)

--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    return view('products');
+});
+
+Route::get('/cart', function () {
+    return view('cart');
+});
+
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
