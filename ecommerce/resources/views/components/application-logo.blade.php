7a79a2cbaa1464e3c93d6c632ffefaadeafe825d
Marnyot <marioardhana37@gmail.com>
2026-08-19 19:12:31 +0700

    style(ecommerce): brand Breeze logo & buttons with Eduwork theme (sesi 15)

---
+++ ecommerce/resources/views/components/application-logo.blade.php
@@ -0,0 +1,13 @@
+<svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
+    <defs>
+        <linearGradient id="edu-logo-g" x1="4" y1="2" x2="36" y2="38" gradientUnits="userSpaceOnUse">
+            <stop offset="0" stop-color="#379966"/>
+            <stop offset="1" stop-color="#1f5d3f"/>
+        </linearGradient>
+    </defs>
+    <rect width="40" height="40" rx="13" fill="url(#edu-logo-g)"/>
+    <rect x="10" y="11" width="18" height="4" rx="2" fill="#fff"/>
+    <rect x="10" y="18" width="13" height="4" rx="2" fill="#fff" fill-opacity="0.92"/>
+    <rect x="10" y="25" width="18" height="4" rx="2" fill="#fff"/>
+    <circle cx="32" cy="9" r="4.5" fill="#d97706"/>
+</svg>
